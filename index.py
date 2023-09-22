9f76bd4ea157c38213c8ba06687edb2e882320fb
Yogimaharaj72 <145778455+Yogimaharaj72@users.noreply.github.com>
2023-09-22 21:06:36 +0530

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = "https://moviesdownlodar-bot.vercel.app"
+URL = "https://moviesdownlodar-bott.vercel.app"
 bot = Bot(TOKEN)
 
 
